18ce6a6e2da923d4dfec71e760cae6d43c31a989
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-07-26 20:03:09 -0500

    prueba Cristian Chulca

---
+++ src/t01_ProgramacionOO/MainPrueba.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MainPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner L = new Scanner (System.in); 
+		
+	Do{
+		System.out.println("Ingrese solo numeros positivos: ");
+		int a = L.nextInt(); 
+	   
+	}while(a <= 0) {
+		
+	}
+	
+	}
+}
+
